1ebbd737b634cd2924822636c6ca0ed8f97056e2
Wei Liu <liuw1619@gmail.com>
2022-11-03 13:52:25 -0400

    add drop_path_rate

--- models/proposed.py
+++ models/proposed.py
@@ -78,22 +78,23 @@ class Attention(nn.Module):
 
 
 class PixelBlock(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4, drop=0., attn_drop=0.):
+    def __init__(self, dim, num_heads, mlp_ratio=4, drop=0., attn_drop=0., drop_path=0.):
         super().__init__()
         self.norm1 = nn.LayerNorm(dim)
         self.attn = Attention(dim, num_heads=num_heads, attn_drop=attn_drop, proj_drop=drop)
         self.norm2 = nn.LayerNorm(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
     def forward(self, x):
-        x = x + self.attn(self.norm1(x))
-        x = x + self.mlp(self.norm2(x))
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
 
 
 class ChannelBlock(nn.Module):
-    def __init__(self, dim, num_heads, patch_size=7, mlp_ratio=4, drop=0., attn_drop=0.):
+    def __init__(self, dim, num_heads, patch_size=7, mlp_ratio=4, drop=0., attn_drop=0., drop_path=0.):
         super().__init__()
         dim_transpose = patch_size*patch_size
         self.head_channel = dim
@@ -104,14 +105,15 @@ class ChannelBlock(nn.Module):
         self.norm2 = nn.LayerNorm(dim_transpose)
         mlp_hidden_dim = int(dim_transpose * mlp_ratio)
         self.mlp = Mlp(in_features=dim_transpose, hidden_features=mlp_hidden_dim, drop=drop)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
     def forward(self, x):
         x = x.transpose(1, 2)
         B_channel, N_channel, C_channel = x.shape
         x = x.view(B_channel, self.per_channel, N_channel//self.per_channel, C_channel)
         x = x.permute(0, 2, 1, 3).contiguous().view(-1, self.per_channel, C_channel)
-        x = x + self.attn(self.norm1(x))
-        x = x + self.mlp(self.norm2(x))
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
 
         B_merge = int(x.shape[0])
         x = x.view(B_channel, B_merge//B_channel, self.per_channel, C_channel)
@@ -135,13 +137,15 @@ class BasicLayer(nn.Module):
             num_heads=num_heads,
             mlp_ratio=mlp_ratio,
             drop=0., 
-            attn_drop=0.) if j % 2 == 0 else ChannelBlock(
+            attn_drop=0.,
+            drop_path=drop_path[j] if isinstance(drop_path, list) else drop_path) if j % 2 == 0 else ChannelBlock(
             dim=dim, 
             num_heads=1,
             mlp_ratio=mlp_ratio,
             patch_size=patch_size,
             drop=0., 
-            attn_drop=0.)
+            attn_drop=0.,
+            drop_path=drop_path[j] if isinstance(drop_path, list) else drop_path)
             for j in range(depth)])
 
 
@@ -228,7 +232,7 @@ class HyperTransformer(nn.Module):
                                         mlp_ratio=mlp_ratios[i_layer], 
                                         drop=0., 
                                         attn_drop=0., 
-                                        drop_path=0.)
+                                        drop_path=dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])])
             self.attention_layers.append(attention_layer)
 
             embedding_layer = TokenEmbedding(in_feature_map_size=img_size,
@@ -292,12 +296,6 @@ class HyperTransformer(nn.Module):
         x = self.head(x)
         return x
 
-
-    # def forward(self, x):
-    #     x = self.forward_features(x)
-    #     x = x.mean(dim=1)
-    #     x = self.head(x)
-    #     return x
 
 def proposed(dataset, patch_size):
     model = None
